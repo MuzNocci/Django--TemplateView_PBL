bb312e6500b70a6a78c0ca46e19a6edf58d3085e
Müller Nocciolli <muller.nocciolli@gmail.com>
2024-02-28 13:08:07 -0300

    Create "otherpage3" and configured the links

--- app/views.py
+++ app/views.py
@@ -14,7 +14,7 @@ class OtherView(TemplateView):
     template_name = 'otherpage.html'
 
 
-    def get_context_data(self, **kwargs) -> dict[str, Any]:
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
 
         context = super().get_context_data(**kwargs)
 
